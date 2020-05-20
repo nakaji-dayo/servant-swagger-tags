d64825fa39418e76f42d81c5529a319a6fbfe013
dustinnorwood <dnorwood2010@aol.com>
2020-05-20 15:04:52 -0400

    Added HasClient instance

--- src/Servant/Swagger/Tags.hs
+++ src/Servant/Swagger/Tags.hs
@@ -16,11 +16,17 @@ import qualified Data.Text       as Text
 import           Data.Typeable   (Typeable)
 import           GHC.TypeLits    (KnownSymbol, Symbol, symbolVal)
 import           Servant         hiding (Context)
+import           Servant.Client
 import           Servant.Mock
 import           Servant.Swagger
 
 data Tags (sym :: Symbol)
     deriving (Typeable)
+
+instance HasClient m api => HasClient m (Tags tags :> api) where
+  type Client m (Tags tags :> api) = Client m api
+  clientWithRoute pm _ = clientWithRoute pm (Proxy :: Proxy api)
+  hoistClientMonad pm _ f cl = hoistClientMonad pm (Proxy :: Proxy api) f cl
 
 instance HasServer api ctx => HasServer (Tags tags :> api) ctx where
   type ServerT (Tags tags :> api) m = ServerT api m
